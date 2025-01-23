d5f8f6bb5ce7cc1d4ccec6535340e677209b586a
bandeanlorand <bandeanlori@gmail.com>
2025-01-23 12:50:50 +0200

    updated Vite, downgraded sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/oscloud-design/' : '/',
   plugins: [react()],
+  logLevel: 'error', // Only show errors
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true, // Suppress deprecation warnings from dependencies
+      },
+    },
+  },
 }));
 
