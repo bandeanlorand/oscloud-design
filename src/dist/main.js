1b9069b33183b25c327d73f8e3f1480213c3ddeb
bandeanlorand <bandeanlori@gmail.com>
2025-01-23 11:50:33 +0200

    sass styling added to project

---
+++ src/dist/main.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var client_1 = require("react-dom/client");
+require("./index.css");
+var router_1 = require("./router");
+var react_router_dom_1 = require("react-router-dom");
+require("bootstrap/scss/bootstrap.scss");
+require("bootstrap/dist/js/bootstrap.bundle.min.js");
+require("./assets/scss/main.scss");
+client_1["default"].createRoot(document.getElementById("root")).render(react_1["default"].createElement(react_1["default"].StrictMode, null,
+    react_1["default"].createElement(react_router_dom_1.RouterProvider, { router: router_1["default"] })));
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/scss/bootstrap.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
